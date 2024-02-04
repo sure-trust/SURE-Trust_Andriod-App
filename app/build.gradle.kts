5713b932d733ffba4d336f287582a9032d1b9b74
Subhadiptech <subhadipdhn@gmail.com>
2024-02-04 15:14:31 +0530

    pushing new revamped app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.suretrustapp.suretrust"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
+        versionCode = 3
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
